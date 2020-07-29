7fbaaba233ad25275af23063aeb5c281c7eb4b04
VladimirGorbachuk <vladimirgorbachuk@gmail.com>
2020-07-29 19:37:01 +0300

    some changes to Review model - added image resizing, added Review model to admin page

---
+++ guest_book/reviews/image_sizes.py
@@ -0,0 +1,2 @@
+IMAGE_SIZE = (400,400,)
+# an image size written as constant. For use in image resizing (if needed)
--- guest_book/reviews/models.py
+++ guest_book/reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from image_sizes import IMAGE_SIZE
 
 class Review(models.Model):
     '''
@@ -10,3 +11,13 @@ class Review(models.Model):
     image = models.ImageField(
         default=None, upload_to='review_pics', blank = True)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    def save(self, *args, **kwargs):
+        # we resize images before saving for uniform at frontend and to exclude
+        # cases with extremely large image files uploaded
+        super().save(*args, **kwargs)
+
+        if self.image:
+            img = Image.open(self.image.path)
+            img.thumbnail(IMAGE_SIZE)
+            img.save(self.image.path)
